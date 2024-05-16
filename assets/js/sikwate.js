c2797957f9e1c89ce09f2faa146a962e0e8c3125
arielsegumpan <arielsegumpan31@gmail.com>
2024-05-16 13:15:22 +0800

    add product page

--- assets/js/sikwate.js
+++ assets/js/sikwate.js
@@ -27,43 +27,13 @@
                     center:false,
                 },
                 1200:{
-                    items:4
+                    items:3 ,
+                    // margin: 50
                 }
             }
         })
         
-        // featured products
-        $('#prod_featured_carousel.owl-carousel').owlCarousel({
-            loop:true,
-            margin:20,
-            nav:false,
-            center:true,
-            dots:false,
-            autoplayTimeout: 10000,
-            autoplay:true,
-            fluidSpeed: true,
-            autoplaySpeed: 6000,
-            navText : false,
-            fluidSpeed:true,
-            responsive:{
-                0:{
-                    items:1,
-                    margin: 30
-                },
-                600:{
-                    items:2
-                },
-                768:{
-                    items:3,
-                    center:false,
-                },
-                1200:{
-                    items:4
-                }
-            }
-        })
-
-        var owl = $('#prod_featured_carousel.owl-carousel');
+        var owl = $('#featured_prod_slide.owl-carousel');
         owl.owlCarousel();
         // Go to the next item
         $('#nextBtn').click(function() {
@@ -75,6 +45,52 @@
             // Parameters has to be in square bracket '[]'
             owl.trigger('prev.owl.carousel', [1500]);
         })
+
+
+
+        // // featured products
+        // $('#prod_featured_carousel.owl-carousel').owlCarousel({
+        //     loop:true,
+        //     margin:20,
+        //     nav:false,
+        //     center:true,
+        //     dots:false,
+        //     autoplayTimeout: 10000,
+        //     autoplay:true,
+        //     fluidSpeed: true,
+        //     autoplaySpeed: 6000,
+        //     navText : false,
+        //     fluidSpeed:true,
+        //     responsive:{
+        //         0:{
+        //             items:1,
+        //             margin: 30
+        //         },
+        //         600:{
+        //             items:2
+        //         },
+        //         768:{
+        //             items:3,
+        //             center:false,
+        //         },
+        //         1200:{
+        //             items:4
+        //         }
+        //     }
+        // })
+
+        // var owl = $('#prod_featured_carousel.owl-carousel');
+        // owl.owlCarousel();
+        // // Go to the next item
+        // $('#nextBtn').click(function() {
+        //     owl.trigger('next.owl.carousel',[1500]);
+        // })
+        // // Go to the previous item
+        // $('#prevBtn').click(function() {
+        //     // With optional speed parameter,
+        //     // Parameters has to be in square bracket '[]'
+        //     owl.trigger('prev.owl.carousel', [1500]);
+        // })
 
     })
 })(jQuery);
